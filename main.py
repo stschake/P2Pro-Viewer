3731e8c1d6cfdfda717755a217f9b8e6777aa349
Stefan Schake <stschake@gmail.com>
2023-12-21 18:00:39 +0100

    add test code for pseudo colors & shutter

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import P2Pro.P2Pro_cmd as P2Pro_CMD
 import P2Pro.recorder
 
 logging.basicConfig()
-logging.getLogger('P2Pro.recorder').setLevel(logging.INFO)
-logging.getLogger('P2Pro.P2Pro_cmd').setLevel(logging.INFO)
+logging.getLogger('P2Pro.P2Pro_cmd').setLevel(logging.DEBUG)
+logging.getLogger('P2Pro.video').setLevel(logging.DEBUG)
 
 try:
     vid = P2Pro.video.Video()
@@ -19,7 +19,7 @@ try:
     while not vid.video_running:
         time.sleep(0.01)
 
-    rec = P2Pro.recorder.VideoRecorder(vid.frame_queue[1], "test")
+    rec = P2Pro.recorder.VideoRecorder(vid.frame_queue[1], "test", audio=False)
     rec.start()
 
     cam_cmd = P2Pro_CMD.P2Pro()
@@ -28,21 +28,31 @@ try:
     # cam_cmd._standard_cmd_write(P2Pro_CMD.CmdCode.sys_reset_to_rom)
     # print(cam_cmd._standard_cmd_read(P2Pro_CMD.CmdCode.cur_vtemp, 0, 2))
     # print(cam_cmd._standard_cmd_read(P2Pro_CMD.CmdCode.shutter_vtemp, 0, 2))
+
+    time.sleep(1)
     cam_cmd.pseudo_color_set(0, P2Pro_CMD.PseudoColorTypes.PSEUDO_IRON_RED)
-    print(cam_cmd.pseudo_color_get())
-    # cam_cmd.set_prop_tpd_params(P2Pro_CMD.PropTpdParams.TPD_PROP_GAIN_SEL, 0)
-    print(cam_cmd.get_prop_tpd_params(P2Pro_CMD.PropTpdParams.TPD_PROP_GAIN_SEL))
-    print(cam_cmd.get_device_info(P2Pro_CMD.DeviceInfoType.DEV_INFO_GET_PN))
+    time.sleep(1)
+    for prop in P2Pro_CMD.AutoShutterParams:
+        print(f"param {prop.name}: {cam_cmd.get_auto_shutter_params(prop)}")
 
-    time.sleep(5)
+    #time.sleep(3)
+    #print("=== shutter test ===")
+    #cam_cmd.get_shutter_status()
+    #cam_cmd.set_shutter(1)
+    #cam_cmd.set_shutter_control(0)
+    #cam_cmd.get_shutter_status()
+    #time.sleep(2)
+    #cam_cmd.set_shutter_control(1)
+    #cam_cmd.set_shutter(0)
+    #cam_cmd.get_shutter_status()
+    #print("=====================")
+
+    time.sleep(3)
     rec.stop()
-
-    while True:
-        # print(vid.frame_queue[0].get(True, 2)) # test
-        time.sleep(0.1)
+    os._exit(1)
 
 except KeyboardInterrupt:
     print("Killing...")
-    time.sleep(5)
+    os._exit(1)
     pass
 os._exit(0)
